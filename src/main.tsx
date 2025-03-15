5bea1dbeaf164da1506a2e4f480394f54a221ffa
Yudho Prakoso <107313576+yudopr11@users.noreply.github.com>
2025-03-15 23:28:08 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,43 +3,54 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { registerSW } from 'virtual:pwa-register'
+import toast from 'react-hot-toast'
 
-// implementation of service worker with auto update
-// with better auto update support
 const updateSW = registerSW({
-  // When new content is available
   onNeedRefresh() {
-    if (confirm('New content available. Reload?')) {
-      updateSW(true)
-    }
+    toast(
+      (t) => (
+        <span style={{ color: 'white' }}>
+          Versi baru aplikasi tersedia. 
+          <button 
+            onClick={() => { 
+              updateSW(true)
+              toast.dismiss(t.id)
+            }} 
+            style={{
+              marginLeft: '8px',
+              background: '#38bdf8',
+              padding: '4px 8px',
+              borderRadius: '4px',
+              color: 'white',
+              border: 'none',
+              cursor: 'pointer'
+            }}>
+            Perbarui
+          </button>
+        </span>
+      ),
+      {
+        duration: Infinity, 
+        style: {
+          borderRadius: '10px',
+          background: '#0f172a',
+          color: '#fff',
+          padding: '16px',
+        },
+      }
+    )
   },
-  // When app is ready to work offline
   onOfflineReady() {
-    // Show message when app is ready to work offline
-    const offlineReady = document.createElement('div')
-    offlineReady.innerHTML = `
-      <div style="
-        position: fixed;
-        bottom: 24px;
-        right: 24px;
-        padding: 16px 24px;
-        background: #0f172a;
-        color: white;
-        font-weight: 500;
-        border-radius: 4px;
-        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-        z-index: 9999;
-        transition: all 0.3s ease;
-      ">
-        App is ready to work offline ✅
-      </div>
-    `
-    document.body.appendChild(offlineReady)
-    
-    // Remove notification after 3 seconds
-    setTimeout(() => {
-      document.body.removeChild(offlineReady)
-    }, 3000)
+    toast('Application is ready to work offline but still requires internet to get data from the server', {
+      icon: '✅',
+      style: {
+        borderRadius: '10px',
+        background: '#0f172a',
+        color: '#fff',
+        padding: '16px',
+      },
+      duration: 4000, // Durasi 4 detik
+    })
   }
 })
 
